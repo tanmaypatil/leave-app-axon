cb3adaffa1c67391092a2389b2769f6543975741
Tanmay Patil <tany.patil77@gmail.com>
2024-04-15 00:03:43 +0530

    basic scaffold of the application

---
+++ leaveapp/src/main/java/com/leaveapp/LeaveApp.java
@@ -0,0 +1,12 @@
+package com.leaveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveApp{
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaveApp.class, args);
+    }
+}
